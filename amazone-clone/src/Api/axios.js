b394e7924647e02b066e9c1fad50d858e547b37a
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-12-08 18:57:11 +0300

    finally done

--- amazone-clone/src/Api/axios.js
+++ amazone-clone/src/Api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+// local instance of firebase functions
+    // baseURL :'http://127.0.0.1:5001/clone-2fb4a/us-central1/api'
 
-    baseURL :'http://127.0.0.1:5001/clone-2fb4a/us-central1/api'
+    // deployed version of Amazon server on render.com
+    baseURL :'https://amazon-back-end-api.onrender.com'
 });
 
 export { axiosInstance };
